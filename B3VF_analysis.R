d1894acc531206c6b8cb674baba8d3b3964ac1f5
Yujia Wang <yw3787@cumc.columbia.edu>
2024-01-04 11:55:55 -0500

    commit all changes

--- B3VF_analysis.R
+++ B3VF_analysis.R
@@ -8,3 +8,9 @@ B3_pt_names <- read_excel("B3 pt names.xlsx")
 #Join the two sheets so that all patients have Study IDs
 B3_102 <- left_join(B3_10_2, B3_pt_names, by = c("Patient First Name", "Patient Last Name", "Patient ID"))
 
+uid <- B3_102 %>% distinct(ID)
+
+write.csv(B3_102, file = "B3_102.csv", row.names = FALSE)
+
+B3_102_all <- read_excel("B3_102_all.xlsx")
+uid <- B3_102_all %>% distinct(`Study ID`)
