8330aed788d644b4f40686f020b7de38ab726ad6
taynagee <tgebhardt011@gmail.com>
2024-01-05 09:07:33 -0500

    selecting only rows we're interested in

--- B3VF_analysis.R
+++ B3VF_analysis.R
@@ -14,3 +14,20 @@ write.csv(B3_102, file = "B3_102.csv", row.names = FALSE)
 
 B3_102_all <- read_excel("B3_102_all.xlsx")
 uid <- B3_102_all %>% distinct(`Study ID`)
+#---------------------------------------------------------------------------------------------------------------------------
+
+
+#Load in spreadsheet with study eye only & visit dates
+B3_102_rta <- read_excel("B3_102_StudyEye_VisitDates.xlsx")
+View(B3_102_rta)
+
+#Converting date column
+
+B3_102_rta$`Perimetry Exam Date` <- as.Date(B3_102_rta$`Perimetry Exam Date`, format = '%m%d%y')
+
+unique_dates <- unique(B3_102_rta[, c("Study ID", "Perimetry Exam Date")])
+
+#Calculate the average `MD in dB` for each patient/ visit
+
+average_MDindB <- aggregate(`MD in dB` ~ `Study ID` + `Perimetry Exam Date`, data = B3_102_rta, FUN = mean)
+
