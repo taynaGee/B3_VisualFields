98b60f18cbed0908d63cf719032fc305bfe6eaa5
taynagee <tgebhardt011@gmail.com>
2024-01-05 12:17:40 -0500

    mary + tayna attempt to find differences

--- New_code_mary.R
+++ New_code_mary.R
@@ -24,3 +24,23 @@ plot(B3_102_limdata1$Avg_MD, B3_102_limdata1$Diff_2_3,
      xlab = "Averages",
      ylab = "Differences")
 abline(lm(B3_102_limdata1$Avg_MD ~ B3_102_limdata1$Diff_2_3))
+
+
+
+#####TEST TEST TEST ################################################
+#Tayna's attempt to find differences and keep in 1 column
+
+# Function to calculate differences for all combinations
+calculate_differences <- function(x) {
+  comb <- combn(x, 2, simplify = TRUE)
+  diffs <- comb[1,] - comb[2,]
+  return(diffs)
+}
+
+# Group by 'StudyID' and 'Visit', then calculate differences for all combinations
+B3_102_limdata <- B3_102_limdata %>%
+  group_by(`Study ID`, Visit) %>%
+  summarise(Diffs_MD = list(calculate_differences(`MD in dB`)))
+
+# Unnest the list column
+B3_102_limdata <- unnest(B3_102_limdata, cols = Diffs_MD)
