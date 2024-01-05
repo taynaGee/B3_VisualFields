92befb65422170c29c8b0705b0140828e0ae842e
taynagee <tgebhardt011@gmail.com>
2024-01-05 10:35:33 -0500

    average calculations

--- New_code_mary.R
+++ New_code_mary.R
@@ -5,4 +5,6 @@ library(tidyverse)
 B3_102_limdata <- B3_102_limdata %>%
   group_by(`Study ID`, Visit) %>%
   mutate(Avg_MD = mean(`MD in dB`, na.rm = TRUE))%>%
-  mutate(Avg_MDi = mean(`MD Inferior in dB`, na.rm = TRUE))
+  mutate(Avg_MDi = mean(`MD Inferior in dB`, na.rm = TRUE))%>%
+  mutate(Avg_MDs = mean(`MD Superior in dB`, na.rm = TRUE))%>%
+  mutate(Avg_PSD = mean(`PSD in dB`, na.rm = TRUE))
