90d266ba93bea37cecfd00d684244d65d6033a48
Yujia Wang <yw3787@cumc.columbia.edu>
2024-01-05 12:12:18 -0500

    Commit changes

--- New_code_mary.R
+++ New_code_mary.R
@@ -8,3 +8,19 @@ B3_102_limdata <- B3_102_limdata %>%
   mutate(Avg_MDi = mean(`MD Inferior in dB`, na.rm = TRUE))%>%
   mutate(Avg_MDs = mean(`MD Superior in dB`, na.rm = TRUE))%>%
   mutate(Avg_PSD = mean(`PSD in dB`, na.rm = TRUE))
+
+
+###TEST TEST TEST ################################################
+B3_102_limdata1 <- B3_102_limdata %>%
+    arrange(`Study ID`, Visit) %>%  
+  group_by(`Study ID`, Visit) %>%
+  mutate(Diff_2_3 = (lead(`MD in dB`) - `MD in dB`),
+    Diff_1_3 = (lead(`MD in dB`, 2) - `MD in dB`)
+  ) %>%
+  ungroup()
+
+plot(B3_102_limdata1$Avg_MD, B3_102_limdata1$Diff_2_3,
+     main = "Scatterplot of Averages vs Differences",
+     xlab = "Averages",
+     ylab = "Differences")
+abline(lm(B3_102_limdata1$Avg_MD ~ B3_102_limdata1$Diff_2_3))
