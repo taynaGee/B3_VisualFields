6c523a6549be04c8325744ecadd065da2f39d7e7
Yujia Wang <yw3787@cumc.columbia.edu>
2024-01-05 10:28:59 -0500

    New code

---
+++ New_code_mary.R
@@ -0,0 +1,8 @@
+library(readxl)
+B3_102_limdata <- read_excel("B3_102_limdata.xlsx")
+
+library(tidyverse)
+B3_102_limdata <- B3_102_limdata %>%
+  group_by(`Study ID`, Visit) %>%
+  mutate(Avg_MD = mean(`MD in dB`, na.rm = TRUE))%>%
+  mutate(Avg_MDi = mean(`MD Inferior in dB`, na.rm = TRUE))
